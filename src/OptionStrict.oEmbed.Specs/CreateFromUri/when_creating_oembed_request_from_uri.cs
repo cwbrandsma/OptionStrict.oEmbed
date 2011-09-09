5704902ce8851a3baf134dab8f8cc0805f268d5a
codeprogression <richard@codeprogression.com>
2011-09-09 09:39:18 -0600

    added specs to verify format parsing

---
+++ src/OptionStrict.oEmbed.Specs/CreateFromUri/when_creating_oembed_request_from_uri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace OptionStrict.oEmbed.Specs.CreateFromUri
+{
+    public class when_creating_oembed_request_from_uri
+    {
+        private Establish context = () =>
+                                 {
+                                     _uri=new Uri(
+                                         "http://www.flickr.com/services/oembed/?url=http%3A//www.flickr.com/photos/bees/2341623661/");
+                                 };
+
+        private Because of = () => _request = _uri.GetOEmbedRequest();
+        private It request_object_should_parse_api = () => _request.Api.ShouldEqual("http://www.flickr.com/services/oembed/");
+        private It request_format_should_be_json = () => _request.Format.ShouldEqual(oEmbedFormat.Json);
+        private static Uri _uri;
+        private static oEmbedRequest _request;
+    }
+}
---
+++ src/OptionStrict.oEmbed.Specs/CreateFromUri/when_creating_oembed_request_from_uri_and_api_contains_xml_format.cs
@@ -0,0 +1,20 @@
+using System;
+using Machine.Specifications;
+
+namespace OptionStrict.oEmbed.Specs.CreateFromUri
+{
+    public class when_creating_oembed_request_from_uri_and_api_contains_xml_format
+    {
+        private Establish context = () =>
+                                        {
+                                            _uri=new Uri(
+                                                "http://www.flickr.com/services/oembed.xml?url=http%3A//www.flickr.com/photos/bees/2341623661/");
+                                        };
+
+        private Because of = () => _request = _uri.GetOEmbedRequest();
+        private It request_object_should_parse_api = () => _request.Api.ShouldEqual("http://www.flickr.com/services/oembed.xml");
+        private It request_object_should_parse_format_as_xml = () => _request.Format.ShouldEqual(oEmbedFormat.Xml);
+        private static Uri _uri;
+        private static oEmbedRequest _request;
+    }
+}
