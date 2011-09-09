b5b40bb9fa5524e8a5e50e1716c00f02c93eec9f
codeprogression <richard@codeprogression.com>
2011-09-09 08:26:41 -0600

    Added extension method to serialize oembedrequest object from uri

--- src/OptionStrict.oEmbed/Extensions.cs
+++ src/OptionStrict.oEmbed/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Web;
 
 namespace OptionStrict.oEmbed
 {
@@ -19,9 +21,44 @@ namespace OptionStrict.oEmbed
             }
         }
 
-        public static bool In<T>(this T item, params T[] list)
+        public static string GetOembedContentType(this oEmbedFormat oEmbedFormat)
+        {
+            switch (oEmbedFormat)
+            {
+                case oEmbedFormat.Xml:
+                    return "text/xml";
+                case oEmbedFormat.Jsonp:
+                    return "application/javascript";
+                default:
+                    return "application/json";
+            }
+        }
+
+        internal static bool In<T>(this T item, params T[] list)
         {
             return list.Contains(item);
+        }
+
+        internal static int? ToInt(this string @string)
+        {
+            if (String.IsNullOrEmpty(@string))
+                return null;
+
+            int result;
+            var success = Int32.TryParse(@string, out result);
+            return success ? (int?)result : null;
+        }
+
+        public static oEmbedRequest GetOEmbedRequest(this Uri uri)
+        {
+            var apiUrl = uri.AbsoluteUri.Replace(uri.Query, "");
+            var queryString = HttpUtility.ParseQueryString(uri.Query);
+            string url;
+            if (queryString.AllKeys.Length == 0 || String.IsNullOrEmpty((url = queryString["url"])))
+                throw new ArgumentException("url argument is required");
+            var request = new oEmbedRequest(apiUrl, url);
+            request.AddQueryParameters(queryString);
+            return request;
         }
     }
 }
